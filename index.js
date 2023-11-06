845a9a7be037fc1e86427567ba524d166691004e
kazamaki76@gmail.com <kazamaki76@gmail.com>
2023-11-06 18:04:53 +0700

    Private message?

--- index.js
+++ index.js
@@ -13,21 +13,30 @@ const socketIO = require("socket.io")(http, {
   },
 });
 app.use(cors());
-
 let users = [];
+let onlineUser = {};
 
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
-  socket.on("message", (data) => {
-    socketIO.emit("messageResponse", data);
-  });
-  socket.on("typing",(data) => socket.broadcast.emit("typingResponse", data))
+  socket.on("send-message", (message, room) => {
+    socket.broadcast.emit("typingResponse", data);
 
+    socket.to(socketID).emit("typingResponse", data);
+  });
+  socket.on("join-room", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("message", (data) => {
+    socket.to(onlineUser[data.to]).emit("messageResponse", data);
+  });
 
   //  Lister when a new user  join the server
   socket.on("newUser", (data) => {
     // add new user to the list of users
-    users.push(data)
+    onlineUser[data.userName] = socket.id;
+    console.log(onlineUser);
+    users.push(data);
     // console.log(users)
     // Send the list of users to the client
     socketIO.emit("newUserResponse", users);
