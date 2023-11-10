0f99b5c8ed208cad2d85e12d0757a1e48d0193ab
kazamaki76@gmail.com <kazamaki76@gmail.com>
2023-11-10 11:13:18 +0700

    Fixed

--- index.js
+++ index.js
@@ -14,51 +14,36 @@ const socketIO = require("socket.io")(http, {
 });
 app.use(cors());
 let users = [];
-let onlineUser = {};
+
 
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
-  socket.on("send-message", (message, room) => {
-    socket.broadcast.emit("typingResponse", data);
+  socket.on("message", data => {
+    socketIO.emit("messageResponse", data)
+  })
 
-    socket.to(socketID).emit("typingResponse", data);
-  });
-  socket.on("join-room", (room) => {
-    socket.join(room);
-  });
+  socket.on("typing", data => (
+    socket.broadcast.emit("typingResponse", data)
+  ))
 
-  socket.on("message", (data) => {
-    socket.to(onlineUser[data.to]).emit("messageResponse", data);
-  });
+  socket.on("newUser", data => {
+    users.push(data)
+    socketIO.emit("newUserResponse", users)
+  })
 
-  //  Lister when a new user  join the server
-  socket.on("newUser", (data) => {
-    // add new user to the list of users
-    onlineUser[data.userName] = socket.id;
-    console.log(onlineUser);
-    users.push(data);
-    // console.log(users)
-    // Send the list of users to the client
-    socketIO.emit("newUserResponse", users);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("🔥: A user disconnected");
-    // Update the kust if ysers when a user disconnect from the server
-    users = users.filter((user) => user.socketID !== socket.id);
-    // console.log(users)
-    // Send the list of users to the client
-    socketIO.emit("newUserResponse", users);
-    socket.disconnect();
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+    users = users.filter(user => user.socketID !== socket.id)
+    socketIO.emit("newUserResponse", users)
+    socket.disconnect()
   });
 });
 
 app.get("/api", (req, res) => {
-  res.json({
-    message: "Hello world",
-  });
+res.json({message: "Hello"})
 });
 
+ 
 http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
